3b51f04eb2a3405d83c9640a82fd6411b5bc628a
Cole Williams <cwilli50@node1323.oscar.ccv.brown.edu>
2023-05-15 13:40:10 -0400

    comment out lines to delete vcf

--- single_sim.sh
+++ single_sim.sh
@@ -14,11 +14,11 @@ bash ${ibdcaller}.sh $pop $rel $iter
 
 python3 pedigree_tools.py ${pop}/${pop}_chr1_${rel}${iter}.txt $pop $rel $iter rename_pop_ibd
 
-if [[ -f ${rel}_${pop}_sim${iter}_segments.f ]]
-then
+#if [[ -f ${rel}_${pop}_sim${iter}_segments.f ]]
+#then
 
-cd ${pop}
+#cd ${pop}
 
-rm ${rel}${iter}_chr*.vcf ${rel}${iter}.seg ${rel}${iter}.vcf ${pop}_unrelateds.vcf
+#rm ${rel}${iter}_chr*.vcf ${rel}${iter}.seg ${rel}${iter}.vcf ${pop}_unrelateds.vcf
 
-fi
+#fi
