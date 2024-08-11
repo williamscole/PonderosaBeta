e605112e002170e42007097c2e8f032947c97441
williamscole <colew@augrabies.hpc.genomecenter.ucdavis.edu>
2024-08-11 16:06:22 -0700

    update debug

--- debug_augrabies.sh
+++ debug_augrabies.sh
@@ -4,12 +4,12 @@ conda activate ped-sim
 
 wdir="/share/hennlab/data/genomes/CAAPA_freeze2_PHASED_common_rare/ponderosa"
 #python3 /share/hennlab/data/genomes/CAAPA_freeze2_PHASED_common_rare/ponderosa/simulations_simplified_2/Ponderosa.py \
-python3 Ponderosa.py
+python3 Ponderosa.py \
     --ibd ${wdir}/batwa/chr1.batwa.snps.h3a.maf01.hg19.phasedIBD.txt \
     --fam ${wdir}/batwa/allchr.batwa.snps.h3a.maf01.hg19.fam \
     --map ${wdir}/interpolated_maps.caapa_h3a_maf01/newchr1.snps.h3a.maf01.hg19.map \
     --training ${wdir}/batwa/Simulations/ponderosa_simulations/degree_classifier_pop1.pkl \
-#    -out ${wdir}/PonderosaBeta/del_batwa.PONDEROSAresults_2 \
-    --output ${wdir}/PonderosaBeta/del_batwa.PONDEROSAresults_2
+    --output ${wdir}/PonderosaBeta/del_batwa.PONDEROSAresults_2 \
     --min_p 0.8 \
+    --king /share/hennlab/data/genomes/CAAPA_freeze2_PHASED_common_rare/ponderosa/batwa/allchr.batwa.snps.h3a.maf01.hg19.ibdseg.seg \
     --debug
